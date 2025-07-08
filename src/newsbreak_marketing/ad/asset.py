d0e858d23c832d029700cf430728d7a18edcf891
Saurabh <sumansaurabh@ideaclan.com>
2025-07-08 18:30:40 +0530

    Refactor upload function to include filename and mime type in asset upload

--- src/newsbreak_marketing/ad/asset.py
+++ src/newsbreak_marketing/ad/asset.py
@@ -1,4 +1,9 @@
+import mimetypes
+import os
+import io
+
 from newsbreak_marketing.utils.api_request import request
+
 async def upload(session, ad_account_id:int|str, media, api_version:str|None = None):
     api_version = api_version or session.api_version
     url = f'{api_version}/ad/uploadAssets'
@@ -12,6 +17,14 @@ async def upload(session, ad_account_id:int|str, media, api_version:str|None = N
         'adAccountId': ad_account_id
     }
 
+    filename = os.path.basename(media.name)
+    mime_type, _ = mimetypes.guess_type(filename)
+    mime_type = mime_type or "application/octet-stream"
+
     files = {
-        'asset': media
+        'asset': (filename, media, mime_type)
     }
+
+    data = await request('POST', url, headers, data=data, files=files)
+
+    return data['assetUrl']
