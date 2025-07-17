7163e7b3208c8d3e703458b71e43738b36c4319b
Saurabh <sumansaurabh@ideaclan.com>
2025-07-17 16:31:34 +0530

    same

--- example/demo/campaign.py
+++ example/demo/campaign.py
@@ -14,13 +14,16 @@ class SMCampaign:
         ad_account_id:strawberry.ID,
         objective:strawberry.enum(CampaignObjective), # type: ignore
     )-> STCampaign:
+        #####################################################################################
+        #####################################################################################
         camp = Campaign(ad_account_id=ad_account_id)
 
         camp = await camp.create(
             name=name,
             objective=objective
         )
-
+        #####################################################################################
+        #####################################################################################
         return STCampaign(
             id=camp.id, # type: ignore
             org_id=camp.org_id, # type: ignore
@@ -37,13 +40,16 @@ class SMCampaign:
         campaign_id:strawberry.ID,
         name:str
     )-> STCampaign:
+        #####################################################################################
+        #####################################################################################
         camp = Campaign(ad_account_id=account_id)
 
         camp = await camp.update(
             campaign_id=campaign_id,
             name=name
         )
-
+        #####################################################################################
+        #####################################################################################
         return STCampaign(
             id=camp.id, # type: ignore
             org_id=camp.org_id, # type: ignore
@@ -60,13 +66,16 @@ class SMCampaign:
         campaign_id:strawberry.ID,
         status:strawberry.enum(Status) # type: ignore
     )-> STCampaign:
+        #####################################################################################
+        #####################################################################################
         camp = Campaign(ad_account_id=account_id)
 
         camp = await camp.update_status(
             campaign_id=campaign_id,
             status=status
         )
-
+        #####################################################################################
+        #####################################################################################
         return STCampaign(
             id=camp.id, # type: ignore
             org_id=camp.org_id, # type: ignore
@@ -82,10 +91,13 @@ class SMCampaign:
         account_id:strawberry.ID,
         campaign_id:strawberry.ID
     )-> STCampaign:
+        #####################################################################################
+        #####################################################################################
         camp = Campaign(ad_account_id=account_id)
 
         camp = await camp.delete(campaign_id=campaign_id)
-
+        #####################################################################################
+        #####################################################################################
         return STCampaign(
             id=camp.id, # type: ignore
             org_id=camp.org_id, # type: ignore
@@ -104,10 +116,13 @@ class SQCampaign:
         ad_account_id:strawberry.ID,
         campaign_id:strawberry.ID
     )-> STCampaign:
+        #####################################################################################
+        #####################################################################################
         camp = Campaign(ad_account_id)
 
         camp = await camp.get(campaign_id=campaign_id)
-
+        #####################################################################################
+        #####################################################################################
         return STCampaign(
             id=camp.id, # type: ignore
             org_id=camp.org_id, # type: ignore
